9c5ca6a8a51fdecd1220edaa27143e7eb386c3d6
Francisca <fvargasserani@gmail.com>
2020-10-24 21:37:21 -0300

    added positive lookahead, lowercase and uppercase letters

--- fuerza_bruta.rb
+++ fuerza_bruta.rb
@@ -1,15 +1,10 @@
-#Definicion de metodo fuerza_bruta
-def fuerza_bruta (pass)
-    if pass != Array(0..9)      
-        if pass.size > 0
-            puts "Se requieren #{27 ** pass.size} intentos."
-        else
-            puts 'Error. Ingrese al menos una letra.'
-        end
+def fuerza_bruta(pass)
+    if pass =~ /^(?=.*\D)[-\w]+$/ #Positive Lookahead
+        puts "Se requieren #{54 ** pass.size} intentos." #27 letras mayusculas y 27 letras minusculas
     else
-        puts 'Error. Ingrese solo letras.'
+        puts 'Error. Ingrese solo letras'
     end
-end 
+end
 
 #utilizando metodo fuerza_bruta
-print fuerza_bruta (ARGV[0])
+fuerza_bruta(ARGV[0])
